9a05135150ef573b4ae0ab6f2d9b5995220ad8c9
ManfredasMi <manfredasmi@gmail.com>
2019-05-10 19:03:35 +0300

    small forms refurb

--- pages/forms.js
+++ pages/forms.js
@@ -1,52 +1,52 @@
 const Forms = function() {
-  const nameFieldBox = $("input[name='name'][required]");
-  const email = $("input[name='email']");
-  const password = $("#exampleInputPassword1");
-  const checkbox = $("#exampleCheck1");
-  const gender = $("#exampleFormControlSelect1").$$("option");
-  //   const gender = element(by.cssContainingText("#exampleFormControlSelect1 option", "Female"))
-  const radioButton = element.all(by.name("inlineRadioOptions"));
-  const submit = element(by.buttonText("Submit"));
-  // const submitMessage = $(".alert strong");
-  const submitMessage = $("div[class*='success']");
-  const nameErrorMessage = $(".form-group .alert");     // alt selector: [class="alert alert-danger"]
+  this.nameFieldBox = $("input[name='name'][required]");
+  this.email = $("input[name='email']");
+  this.password = $("#exampleInputPassword1");
+  this.checkbox = $("#exampleCheck1");
+  this.gender = $("#exampleFormControlSelect1").$$("option");
+  //   this.gender = element(by.cssContainingText("#exampleFormControlSelect1 option", "Female"))
+  this.radioButton = element.all(by.name("inlineRadioOptions"));
+  this.submit = element(by.buttonText("Submit"));
+  // this.submitMessage = $(".alert strong");
+  this.submitMessage = $("div[class*='success']");
+  this.nameErrorMessage = $(".form-group .alert");     // alt selector: [class="alert alert-danger"]
 
   this.inputName = text => {
-    nameFieldBox.sendKeys(text);
+    this.nameFieldBox.sendKeys(text);
   };
 
   this.inputEmail = text => {
-    email.sendKeys(text);
+    this.email.sendKeys(text);
   };
 
   this.inputPassword = text => {
-    password.sendKeys(text);
+    this.password.sendKeys(text);
   };
 
   this.checkCheckbox = () => {
-    checkbox.click();
+    this.checkbox.click();
   };
 
   this.selectGender = number => {
-    gender.get(number).click();
+    this.gender.get(number).click();
   };
 
   this.checkRadioButton = () => {
-    radioButton.first().click();
+    this.radioButton.first().click();
   };
 
   this.clickSubmit = () => {
-    submit.click();
+    this.submit.click();
   };
 
   this.getSuccessMessage = () => {
-    submitMessage.getText().then(text => {
+    this.submitMessage.getText().then(text => {
       console.log(text);
     });
   };
 
   this.validateNameErrorMessage = () => {
-    return nameErrorMessage.getText();
+    return this.nameErrorMessage.getText();
   }
 };
 module.exports = new Forms();
