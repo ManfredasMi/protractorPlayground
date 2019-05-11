ec309308038bceb9e7fe980eaa6f4daa090ab69b
ManfredasMi <manfredasmi@gmail.com>
2019-05-11 20:27:55 +0300

    Added jasmine screenshot reporter

--- conf.js
+++ conf.js
@@ -1,6 +1,8 @@
+const Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
 exports.config = {
   seleniumAddress: "http://localhost:4444/wd/hub",
-  specs: ["specs/forms.spec.js"],
+  specs: ["specs/locatorsMultipleTest.spec.js"],
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
     showColors: true // Use colors in the command line report.
@@ -14,5 +16,10 @@ exports.config = {
       .manage()
       .window()
       .maximize();
+    jasmine.getEnv().addReporter(
+      new Jasmine2HtmlReporter({
+        savePath: "target/screenshots"
+      })
+    );
   }
 };
